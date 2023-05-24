29f24037a51f1d6c10b7bd0f5ebcdc49a3dbfce4
Tudorache Mihai-Ovidiu <ovi99or@gmail.com>
2023-05-24 19:24:05 +0300

    Initial code

---
+++ src/main/java/guru/springframework/msscbrewery/services/BeerSerivceImpl.java
@@ -0,0 +1,17 @@
+package guru.springframework.msscbrewery.services;
+
+import guru.springframework.msscbrewery.web.model.BeerDto;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+
+@Service
+public class BeerSerivceImpl implements BeerSerivce {
+    @Override
+    public BeerDto getBeerById(UUID beerID) {
+        return BeerDto.builder().id(UUID.randomUUID())
+                .beerName("Galaxy Cat")
+                .beerStyle("Pale Ale")
+                .build();
+    }
+}
