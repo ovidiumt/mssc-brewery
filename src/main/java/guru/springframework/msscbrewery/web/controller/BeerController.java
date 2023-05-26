faf307373e9fa39760c31ebf619c31bdbfbca777
Tudorache Mihai-Ovidiu <ovi99or@gmail.com>
2023-05-26 12:29:30 +0300

    small update

--- src/main/java/guru/springframework/msscbrewery/web/controller/BeerController.java
+++ src/main/java/guru/springframework/msscbrewery/web/controller/BeerController.java
@@ -32,9 +32,9 @@ public class BeerController {
 
         HttpHeaders headers = new HttpHeaders();
         //todo add hostname to url
-        headers.add("Location","/api/v1/beer" + savedDto.getId().toString());
+        headers.add("Location","/api/v1/beer/" + savedDto.getId().toString());
 
-        return new ResponseEntity(headers, HttpStatus.OK);
+        return new ResponseEntity(headers, HttpStatus.CREATED);
     }
 
     @PutMapping({"/{beerId"})
