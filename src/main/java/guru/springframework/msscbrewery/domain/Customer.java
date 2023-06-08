a0b77f9f54423fcdf3da14e06812f520a3045a30
Tudorache Mihai-Ovidiu <ovi99or@gmail.com>
2023-06-08 21:01:04 +0300

    added customer mapping

---
+++ src/main/java/guru/springframework/msscbrewery/domain/Customer.java
@@ -0,0 +1,19 @@
+package guru.springframework.msscbrewery.domain;
+
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Customer {
+    private UUID id;
+    private String customerName;
+}
